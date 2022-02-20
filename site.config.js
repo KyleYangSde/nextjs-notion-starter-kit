427d37fc082410868f43856e89e5461bd3c6f50e
kyrie96521 <42157568+kyrie96521@users.noreply.github.com>
2022-02-20 14:17:12 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'TestMyBlog-320e62d381154f31b7618c000673bc45',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Kyle space',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Kyle',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'https://balsam-grill-d62.notion.site/TestMyBlog-320e62d381154f31b7618c000673bc45',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
